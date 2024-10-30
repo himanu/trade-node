61929d62ac0383b565b227d23f86022d538166fb
Himanshu Yadav <hy287719@gmail.com>
2024-10-30 19:39:23 +0530

    update

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    res.send("Hello World");
+})
+
+app.get("/coin", async (req, res) => {
+    try {
+
+    const response = await fetch("https://api.coingecko.com/api/v3/ping");
+    const json = await response.json();
+    console.log("responseponse ", json);
+    res.send(json);
+    } catch (err) {
+        console.log("Error ", err);
+        res.status(500).send(err.message);
+    }
+})
+
+app.listen(5000, () => console.log("Server is running"));
